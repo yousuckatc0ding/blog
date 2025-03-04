29e11e61db9db6635060ed6988b310dc242ca779
akash <yousuckatcoding@proton.me>
2025-03-05 00:52:16 +0530

    Remove posthog logging

--- static/scripts/list_replacing.js
+++ static/scripts/list_replacing.js
@@ -1,11 +1,3 @@
-import posthog from 'posthog-js'
-
-posthog.init('phc_uuLYaEohY65NGpdlF7eOH2dQucCkpkP4wzs7JMsqn4v',
-    {
-        api_host: 'https://us.i.posthog.com',
-        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
-    }
-)
 document.addEventListener("DOMContentLoaded", function() {
     try {
         // Apply Tailwind classes to selected elements
